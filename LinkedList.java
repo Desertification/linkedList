143e9bb1bc441fe8ca0f02ae2fee4dd66d0dd377
Thomas Cauwelier <thomascauwelier@gmail.com>
2017-03-04 22:50:01 +0100

    move git root to make it easier to use as package

---
+++ LinkedList.java
@@ -0,0 +1,156 @@
+package myLinkedList;
+
+/**
+ * Created by thoma on 17-Feb-17.
+ */
+public class LinkedList<T> {
+    private Node head;
+    private int size;
+
+    /**
+     * Constructor of an empty list
+     */
+    public LinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    /**
+     * Constructor for the linked list
+     *
+     * @param element generic
+     */
+    public LinkedList(T element) {
+        head = new Node(element);
+        size = 1;
+    }
+
+    private LinkedList(Node node) {
+        head = node;
+        size = count(head, 0);
+    }
+
+    /**
+     * prepends the element to the linked list
+     *
+     * @param element generic
+     */
+    public void prepend(T element) {
+        head = new Node(element, head);
+        size++;
+    }
+
+    /**
+     * @return the head of the list
+     */
+    public T first() {
+        if (head == null) {
+            return null;
+        } else {
+            return head.get();
+        }
+    }
+
+    /**
+     * @return the last element of the list
+     */
+    public T last() {
+        return lastRecusive(head).get();
+    }
+
+    private Node lastRecusive(Node current) {
+        if (current.next() == null) {
+            return current;
+        }
+        return lastRecusive(current.next());
+    }
+
+    /**
+     * find the element in the list
+     *
+     * @param element to find
+     * @return true if element is in the list
+     */
+    public boolean find(T element) {
+        Node cursor = head;
+        do {
+            if (cursor.get().equals(element)) {
+                return true;
+            }
+        } while (cursor.next() != null);
+        return false;
+    }
+
+    /**
+     * @return the linked list without the head element
+     */
+    public LinkedList<T> tail() {
+        return new LinkedList<T>(head.next());
+    }
+
+    /**
+     * @return true if empty, false if the linked list contains elements
+     */
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private int countRecusive(Node current, int total) {
+        if (current == null) {
+            return 0;
+        } else if (current.next() != null) {
+            total++;
+            return countRecusive(current.next(), total);
+        } else {
+            return total;
+        }
+    }
+
+//    private int count(){
+//        if (head == null){
+//            return 0;
+//        }
+//        int total = 1;
+//        while(head.next() != null){
+//            total++;
+//        }
+//        return total;
+//        return countRecusive(head,1);
+//    }
+
+    // teacher solution
+    private int count(Node current, int total) {
+        if (current == null) {
+            return total;
+        }
+        return count(current.next(), ++total);
+    }
+
+    private class Node {
+        private T element;
+        private Node next;
+
+        public Node(T element) {
+            this(element, null);
+        }
+
+        public Node(T element, Node next) {
+            this.element = element;
+            this.next = next;
+        }
+
+        public T get() {
+            return element;
+        }
+
+        public Node next() {
+            return next;
+        }
+    }
+
+
+}
---
+++ LinkedListTest.java
@@ -0,0 +1,66 @@
+package myLinkedList;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by thoma on 17-Feb-17.
+ */
+class LinkedListTest {
+    LinkedList<String> linkedList;
+
+    @Test
+    void find() {
+        Assert.assertTrue("1", linkedList.find("1"));
+    }
+
+    @Test
+    void last() {
+        Assert.assertEquals("1", linkedList.last());
+        linkedList.prepend("2");
+        Assert.assertEquals("2", linkedList.first());
+        Assert.assertEquals("1", linkedList.last());
+    }
+
+    @Test
+    void tail() {
+        linkedList.prepend("2");
+        LinkedList<String> result = linkedList.tail();
+        Assert.assertEquals("1", result.first());
+        Assert.assertEquals(1, result.size());
+    }
+
+    @BeforeEach
+    void setUp() {
+        linkedList = new LinkedList<String>("1");
+    }
+
+    @org.junit.jupiter.api.Test
+    void testEmptyList() {
+        LinkedList<String> linkedList = new LinkedList<>();
+        Assert.assertEquals(0, linkedList.size());
+        Assert.assertEquals(true, linkedList.isEmpty());
+    }
+
+    @org.junit.jupiter.api.Test
+    void prepend() {
+        linkedList.prepend("2");
+        Assert.assertEquals("2", linkedList.first());
+        Assert.assertEquals(2, linkedList.size());
+    }
+
+    @org.junit.jupiter.api.Test
+    void first() {
+        Assert.assertEquals("1", linkedList.first());
+        Assert.assertEquals(1, linkedList.size());
+        Assert.assertEquals(false, linkedList.isEmpty());
+    }
+
+    @org.junit.jupiter.api.Test
+    void testTailWithOneElement() {
+        LinkedList<String> result = linkedList.tail();
+        Assert.assertEquals(null, result.first());
+        Assert.assertEquals(0, result.size());
+    }
+}
