40b7828f9d8ac81a01c7846d298d95e3d42fbc70
Thomas Cauwelier <thomascauwelier@gmail.com>
2017-03-20 00:18:53 +0100

    add javadoc

--- Iterator.java
+++ Iterator.java
@@ -2,9 +2,16 @@ package myLinkedList;
 
 /**
  * Created by thoma on 14-Mar-17.
+ * interface to enable easy iteration of custom collection
  */
 public interface Iterator<T> {
+    /**
+     * @return true if there is still an object int the collection
+     */
     boolean hasNext();
 
+    /**
+     * @return the next object in the collection
+     */
     T next();
 }
