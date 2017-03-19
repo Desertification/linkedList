cfca7fbec6c1a7752c03242747969b7fba4f75b1
Thomas Cauwelier <thomascauwelier@gmail.com>
2017-03-19 23:02:07 +0100

    add todo

--- LinkedListIterator.java
+++ LinkedListIterator.java
@@ -2,6 +2,7 @@ package myLinkedList;
 
 /**
  * Created by thoma on 14-Mar-17.
+ * todo fix iterating and removing can cause nulpointer exception (fixable?)
  */
 public class LinkedListIterator<T> implements Iterator<T> {
     private LinkedList<T> linkedList;
